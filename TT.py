5b27cd36548d5d3f4a6cd43e897e264d34392d53
Yichi Chen <ychen@localhost.localdomain>
2014-05-01 13:33:14 -0400

    Added a draggable paddle and one side working relfection

--- TT.py
+++ TT.py
@@ -5,6 +5,11 @@ from BaseGameEntity import *
 from Renderer import *
 from GamePhysics import CollisionDetection
 
+def drag(event):
+    pad.pos = Vec2D(event.x, event.y)
+
+
+
 root = Tk()
 cd = CollisionDetection.CollisionTests()
 can = Canvas(root, bg="white", width=600, height=600)
@@ -15,11 +20,14 @@ circle = [Vec2D(10 * sqrt(3) / 2, 10 * .5), Vec2D(10 * sqrt(2) / 2, 10 * sqrt(2)
                  Vec2D(10 * -sqrt(3) / 2, 10 * -.5), Vec2D(10 * -sqrt(2) / 2, 10 * -sqrt(2) / 2), Vec2D(10 * -.5, 10 * -sqrt(3) / 2), Vec2D(0, -10),
                   Vec2D(10 * .5, 10 * -sqrt(3) / 2), Vec2D(10 * sqrt(2) / 2, 10 * -sqrt(2) / 2), Vec2D(10 * sqrt(3) / 2, 10 * -.5), Vec2D(10, 0)]
 rect_vertices = [Vec2D(-10, -10), Vec2D(-10, 10), Vec2D(10, 10), Vec2D(10, -10)]
-cir = BaseGameEntity(circle, pos=Vec2D(100, 100), velocity=Vec2D(230, 150), angularVelocity=1)
-rect = BaseGameEntity(rect_vertices, pos=Vec2D(500, 500), velocity=Vec2D(-200, -200), angularVelocity=1)
+cir = BaseGameEntity(circle, pos=Vec2D(150, 100), velocity=Vec2D(230, 150), angularVelocity=3)
+rect = BaseGameEntity(rect_vertices, pos=Vec2D(300, 100), velocity=Vec2D(-200, -200), angularVelocity=3)
 
+pad_v = [Vec2D(-30, -10), Vec2D(-30, 10), Vec2D(30,10), Vec2D(30,-10)]
+pad = BaseGameEntity(pad_v, pos=Vec2D(300, 500))
 renderer.addEntity(cir)
 renderer.addEntity(rect)
+renderer.addEntity(pad)
 
 horizontal = [Vec2D(-5, -5), Vec2D(-5, 5), Vec2D(495, 5), Vec2D(495, -5)]
 vertical = [Vec2D(-5, -5), Vec2D(-5, 505), Vec2D(5, 505), Vec2D(5, -5)]
@@ -34,8 +42,11 @@ renderer.addEntity(bot)
 renderer.addEntity(left)
 renderer.addEntity(right)
 
-entities = [rect, cir, top, bot, left, right]
+entities = [rect, cir, top, bot, left, right, pad]
 can.pack()
+
+
+root.bind("<B1-Motion>", drag)
 
 
 while True:
@@ -43,6 +54,12 @@ while True:
     time.sleep(deltaTime)
     for e in entities:
         e.update(deltaTime)
+        
+    m = cd.testCollisionSAT(cir, pad)
+    if m:
+        cir.pos += m
+        if cir.velocity == Vec2D(230, 150) or cir.velocity == Vec2D(-230, 150):
+            cir.velocity = -cir.velocity.getReflection(Vec2D(0, -1))
   
     mtv = cd.testCollisionSAT(cir, rect)
     if(mtv):
